f2fab8720e1c41a7b5ef0c4b1b4f8b5a6276e7e7
csteinmetz1 <csteinmetz1@gmail.com>
2019-03-04 15:31:01 -0500

    working on 1/3 octave band analysis - not fully working yet - dutc

--- README.md
+++ README.md
@@ -30,3 +30,7 @@ python scripts/analyze.py path/to/audio/file.wav
 A new regulation from the National Highway Traffic Safety Administration has been enacted that requires all electric vehicles meet given sound output requirements when operating in different scenarios. This was due to concerns that the low sound output of many electric vehicles can make them hazardous to pedestrians. The regulation provides target sound pressure levels (A weighted) over the frequency range for a number of different operating conditions of the vehicles (stationary, reverse, forward motion, etc.). Full details of the specification are presented in the document below.
 
 [Federal Motor Vehicle Safety Standard No. 141, Minimum Sound Requirements for Hybrid and Electric Vehicles](https://www.federalregister.gov/documents/2018/02/26/2018-03721/federal-motor-vehicle-safety-standard-no-141-minimum-sound-requirements-for-hybrid-and-electric)
+
+## Resources 
+[1/3 octave band analysis by FFT](http://www.mstarlabs.com/docs/tn257.html)
+http://www.ni.com/pdf/manuals/320952a.pdf
--- scripts/analyze.py
+++ scripts/analyze.py
@@ -3,11 +3,11 @@ import soundfile as sf
 import scipy.fftpack as fftpack
 import matplotlib.pyplot as plt
 
-sec = 0.1 # number of seconds to analyze
+block_size = 65536 # number of seconds to analyze
 
 # load in data
-x, fs = sf.read("audio/engine.wav")
-x = x[:int(fs*sec),0] 	# take the left channel and sec seconds
+x, fs = sf.read("audio/engine_16-48k.wav")
+x = x[:block_size,0] 	# take the left channel and block_size samples
 ts = 1.0/fs 			# sampling period		(seconds)
 n = x.shape[0]			# number of samples 
 t = n/fs				# length of signal		(seconds)
@@ -26,15 +26,36 @@ print(f"Loaded {x.shape[0]} samples with fs = {fs}\n")
 # perform frequency domain analysis
 y = fftpack.fft(x)/n
 y = y[range(n//2)]
+m = abs(y)
+
+# divide into 1/3 octave bands
+# ANSI 1/3 octave bands 11-37
+# (12.5 Hz - 5000 Hz)
+bands = np.arange(10,38)
+octaves = (2 ** ((bands-30)/3))*(1000)
+
+power = []
+step = 1
+nbins = 2
+stop = 11 # starting position
+for idx, band in enumerate(bands):
+	if (idx != 0) and (idx % 3 == 0):
+		step *= 2
+	nbins += step
+	start = stop + 1
+	stop  = start + nbins - 1
+	with open("f.txt", "a") as f:
+		f.write(f"{band} {start} {stop} {frq[start:stop+1]} {nbins}\n")
+	power.append(np.sum(m[start:stop]))
 
 # plot the results
 fig, ax = plt.subplots(2, 1)
 ax[0].plot(tv,x)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
-ax[1].plot(frq,np.abs(y),'r')
-ax[1].set_yscale('log')
-ax[1].set_xscale('log')
+ax[1].plot(octaves,power,'r')
+#ax[1].set_yscale('log')
+#ax[1].set_xscale('log')
 ax[1].set_xlabel('Freq (Hz)')
 ax[1].set_ylabel('|Y(freq)|')
 
